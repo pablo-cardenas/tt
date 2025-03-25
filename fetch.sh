3c9ad2bd615ead091a3bdcfe4190bb36bbd76b0a
Pablo Cárdenas <pablo.cardenas@imca.edu.pe>
2025-03-25 13:20:46 -0500

    Updated fetch.sh to use XDG base dirs

--- fetch.sh
+++ fetch.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-if [ ! -f data/monkeytype/quotes_english.json ]; then
-	mkdir -p data/monkeytype
+CACHE="${XDG_CACHE_HOME:-${HOME}/.cache}/tt"
+
+if [ ! -f "${CACHE}/monkeytype/quotes_english.json" ]; then
+	mkdir -p ${CACHE}/monkeytype
 	curl https://monkeytype.com/quotes/english.json \
-		-o data/monkeytype/quotes_english.json
-	echo "Downloaded data/monkeytype/quotes_english.json"
+		-o ${CACHE}/monkeytype/quotes_english.json
+	echo "Downloaded ${CACHE}/monkeytype/quotes_english.json"
 else
-	echo "Existing data/monkeytype/quotes_english.json"
+	echo "Existing ${CACHE}/monkeytype/quotes_english.json"
 fi
